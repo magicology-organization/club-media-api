04c23736ccc3ef8bbe10fe65ce5893d4faecbc55
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-04-05 22:23:31 +0700

    added security pratices

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const connection = require("./config/dbConfig");
-const app = express();
+const helmet = require("helmet");
 
+const app = express();
+app.use(helmet());
+// avoid being attacked by common HTTP
+app.disable("x-powered-by");
+// reduce fingerprints
+
+//routes defines (to be seperated)
 const authEmployeeRoutes = require("./routes/authEmployeeRoutes");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  // Check if the request is attempting to redirect
+  if (req.url === "/" || req.url === "/api") {
+    // Handle the redirect request
+    res.status(403).send("Redirects are not allowed");
+  } else {
+    // Continue to the next middleware or route handler
+    next();
+  }
+});
 app.use("/api/auth", authEmployeeRoutes);
 
 app.listen(PORT, () => {
